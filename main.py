41e23cf975a36c4b768e8e136f1eb8d4a436a8a2
Yang Yan <yangawesome@gmail.com>
2019-03-29 09:12:30 -0400

    better liking system

--- main.py
+++ main.py
@@ -41,30 +41,30 @@ driver.implicitly_wait(0)
 # login
 driver.get("https://www.instagram.com/")
 driver.find_element_by_link_text("Log in").click()
-time.sleep(3)
+time.sleep(2)
 driver.find_element_by_name("username").send_keys(config["username"])
 driver.find_element_by_name("password").send_keys(config["password"])
-time.sleep(3)
-# TODO: this sometimes fails...
+time.sleep(1)
 driver.find_element_by_xpath("//button[@type=\"submit\"]").click()
+time.sleep(5)
 
-# scoll pages and heart posts until many current pages contain no more hearts
+article = driver.find_element_by_css_selector("article")
 windowHeight = driver.execute_script(
     "return window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight")
-while True:
-    likes = driver.find_elements_by_css_selector(
-        "span[aria-label=\"Like\"]")
-    if len(likes) == 0:
-        break
-
-    for a in range(len(likes)):
-        # not a comment
-        if not "glyphsSpriteComment_like" in likes[a].get_attribute("class").split(" "):
-            # scroll element into the middle of page
-            driver.execute_script("window.scrollBy(0, arguments[0]);", driver.execute_script(
-                "return arguments[0].getBoundingClientRect().top;", likes[a]) - windowHeight / 2)
-            likes[a].click()
-            time.sleep(1)
+likedAlready = 0
+while likedAlready < 5:
+    driver.execute_script("window.scrollBy(0, arguments[0]);", driver.execute_script(
+        "return (arguments[0].getBoundingClientRect().top + arguments[0].getBoundingClientRect().bottom) / 2;", article) - windowHeight / 2)
+    time.sleep(1)
+    likes = article.find_elements_by_css_selector("span[aria-label=\"Like\"]")
+    if len(likes) == 0 or "glyphsSpriteComment_like" in likes[0].get_attribute("class").split(" "):
+        likedAlready += 1
+        print(article, "was liked already")
+    else:
+        likes[0].click()
+        print(article, "liked!")
+    article = driver.execute_script(
+        "return arguments[0].nextElementSibling;", article)
 
 # clean up
 driver.get("https://www.instagram.com/")  # to give window time to close
