f6bc9286c40bbb2cd63d62bfc5339e156dbe397c
Yang Yan <~@gilgamesh.cc>
2020-01-03 21:00:09 -0700

    standardize options to command line and add more options

--- main.py
+++ main.py
@@ -1,122 +1,168 @@
-import selenium
-from selenium import webdriver
-import time
-import configparser
-import io
-import traceback
-import zipfile
-import requests
-import getpass
-import getopt
 import sys
+import getopt
+import getpass
+import requests
+import zipfile
+import traceback
+import io
+import time
+from selenium import webdriver
+import selenium
+import pathlib
+
+# use the correct chromedriver for your chrome version!
+CHROMEDRIVER_URL = "https://chromedriver.storage.googleapis.com/79.0.3945.36/chromedriver_win32.zip"
 
 
-def run():
-    CHROMEDRIVER_DIR = ".cache/"
+def main(argv):
+    """
+    Default behavior for the auto-liker when it is run from the command line.
+    Automatically likes posts from Instagram feed.
 
-    # parse configuration
-    config = configparser.ConfigParser()
-    config.read("config.ini")
-    config = config["default"]
-
-    # get username and password from command line or prompt
-    username = None
-    password = None
+    argv: List of command line arguments (sys.argv[1:])
+    """
+    # parse command line arguments
     try:
         opts, args = getopt.getopt(
-            sys.argv[1:], "", ["username=", "password="])
-        for opt in opts:
-            if opt[0] == "--username":
-                username = opt[1]
-            if opt[0] == "--password":
-                password = opt[1]
+            argv, "", ["username=", "password=", "stop-condition=", "period=", "headless"])
+        opts = {opt[0]: opt[1] for opt in opts}
     except:
-        traceback.print_exc()
-    if username is None:
-        username = input("Instagram username: ")
-    if password is None:
-        password = getpass.getpass("Instagram password: ")
+        print("Error parsing command-line arguments. Continuing...")
+        opts = {}
+
+    # get username and password
+    username = opts["--username"] if "--username" in opts.keys() else \
+        input("Instagram username: ")
+    password = opts["--password"] if "--password" in opts.keys() else \
+        getpass.getpass("Instagram password: ")
 
     # download chromedriver
-    chromedriver_file = CHROMEDRIVER_DIR + "chromedriver.exe"
+    CACHE_DIR = ".cache/"
+
     try:
-        request = requests.get(config["chromedriver"])
+        request = requests.get(CHROMEDRIVER_URL)
         archive = zipfile.ZipFile(io.BytesIO(request.content))
 
         # extract first file from the archive
         file = archive.namelist()[0]
-        chromedriver_file = archive.extract(file, CHROMEDRIVER_DIR)
+        chromedriver_file = archive.extract(file, CACHE_DIR)
         archive.close()
         print("Downloaded and extracted chromedriver to", chromedriver_file + ".")
     except:
         traceback.print_exc()
+        chromedriver_file = CACHE_DIR + "chromedriver.exe"
         print("Exception while extracting chromedriver. Using default location of",
               chromedriver_file + ".")
 
-    # setup driver
-    USER_DATA_DIR = ".cache/chrome-user-data/"
-    chrome_options = selenium.webdriver.chrome.options.Options()
-    chrome_options.add_argument("--user-data-dir=" + USER_DATA_DIR)
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-notifications")
-    chrome_options.add_argument("--mute-audio")
-    chrome_options.add_argument("--log-level=3")
-    chrome_options.add_argument("--silent")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--allow-insecure-localhost")
-    chrome_options.add_argument("--disable-extensions")
-    if config["headless"] == "true":
-        chrome_options.add_argument("--headless")
-    driver = selenium.webdriver.Chrome(
-        chromedriver_file, options=chrome_options)
-    driver.implicitly_wait(0)
+    # parse stop-condition
+    stop_condition = int(opts["--stop-condition"]) \
+        if "--stop-condition" in opts.keys() else 5  # defualt value
 
-    try:
-        # login
-        driver.get("https://www.instagram.com/")
-        time.sleep(5)
+    # parse period
+    period = int(opts["--period"]) if "--period" in opts.keys() else None
+
+    # run the script forever! unless period is not set, which we check at the end
+    while True:
+        print("Beginning auto-liker")
+
+        # setup driver
+        try:
+            USER_DATA_DIR = CACHE_DIR + "chrome-user-data/"
+            chrome_options = selenium.webdriver.chrome.options.Options()
+            chrome_options.add_argument("--user-data-dir=" + USER_DATA_DIR)
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument("--window-size=1920,1080")
+            chrome_options.add_argument("--disable-notifications")
+            chrome_options.add_argument("--mute-audio")
+            chrome_options.add_argument("--log-level=3")
+            chrome_options.add_argument("--silent")
+            chrome_options.add_argument("--disable-gpu")
+            chrome_options.add_argument("--allow-insecure-localhost")
+            chrome_options.add_argument("--disable-extensions")
+            if "--headless" in opts.keys():
+                chrome_options.add_argument("--headless")
+
+                # fix! must be set to some unused port
+                chrome_options.add_argument("--remote-debugging-port=9222")
+            driver = selenium.webdriver.Chrome(
+                chromedriver_file, options=chrome_options)
+            driver.implicitly_wait(0)
+            print("Launched driver.")
+        except:
+            traceback.print_exc()
+            print("Failed to launch driver.")
+            return
+
+        # like posts!
+        SCREENSHOT_DIR = CACHE_DIR + "screenshots/"
+        pathlib.Path(SCREENSHOT_DIR).mkdir(parents=True, exist_ok=True)
 
         try:
-            driver.find_element_by_link_text("Log in").click()
-            time.sleep(2)
-            driver.find_element_by_name("username").send_keys(username)
-            driver.find_element_by_name("password").send_keys(password)
-            time.sleep(1)
-            driver.find_element_by_xpath("//button[@type=\"submit\"]").click()
+            driver.get("https://www.instagram.com/")
             time.sleep(5)
+            driver.save_screenshot(SCREENSHOT_DIR + "instagram.png")
+
+            # login
+            try:
+                driver.find_element_by_link_text("Log in").click()
+                time.sleep(5)
+                driver.save_screenshot(SCREENSHOT_DIR + "login.png")
+                driver.find_element_by_name("username").send_keys(username)
+                driver.find_element_by_name("password").send_keys(password)
+                time.sleep(3)
+                driver.find_element_by_xpath(
+                    "//button[@type=\"submit\"]").click()
+                time.sleep(5)
+                print("Logged in.")
+            except:
+                # if profile is being used, then we won't need to login
+                print("Did not log in (perhaps we are already logged in?).")
+
+            driver.save_screenshot(SCREENSHOT_DIR + "feed.png")
+            article = driver.find_element_by_css_selector("article")
+            windowHeight = driver.execute_script(
+                "return window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight")
+            consecutive_skipped = 0
+            while consecutive_skipped < stop_condition:
+                driver.execute_script("window.scrollBy(0, arguments[0]);", driver.execute_script(
+                    "return (arguments[0].getBoundingClientRect().top + arguments[0].getBoundingClientRect().bottom) / 2;", article) - windowHeight / 2)
+                time.sleep(3)
+                likes = article.find_elements_by_css_selector(
+                    "span[aria-label=\"Like\"]")
+
+                if len(likes) == 0 or "glyphsSpriteComment_like" in likes[0].get_attribute("class").split(" "):
+                    consecutive_skipped += 1
+                    print("Skipped post (" + str(consecutive_skipped),
+                          "skipped in a row).")
+                else:
+                    consecutive_skipped = 0
+                    likes[0].click()
+                    print("Liked a post.")
+
+                article = driver.execute_script(
+                    "return arguments[0].nextElementSibling;", article)
+
+            # give window time to send all relevant likes to backend
+            time.sleep(3)
         except:
-            # if profile is being used, then we won't need to login
-            print("Did not log in. Perhaps user is already logged in?")
+            traceback.print_exc()
+        finally:
+            try:
+                driver.close()
+                print("Closed driver.")
+            except:
+                print("Failed to close driver. Finishing...")
 
-        article = driver.find_element_by_css_selector("article")
-        windowHeight = driver.execute_script(
-            "return window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight")
-        likedAlready = 0
-        while likedAlready < 5:
-            driver.execute_script("window.scrollBy(0, arguments[0]);", driver.execute_script(
-                "return (arguments[0].getBoundingClientRect().top + arguments[0].getBoundingClientRect().bottom) / 2;", article) - windowHeight / 2)
-            time.sleep(1)
-            likes = article.find_elements_by_css_selector(
-                "span[aria-label=\"Like\"]")
-            if len(likes) == 0 or "glyphsSpriteComment_like" in likes[0].get_attribute("class").split(" "):
-                likedAlready += 1
-                print(article, "was liked already")
-            else:
-                likedAlready = 0
-                likes[0].click()
-                print(article, "liked!")
-            article = driver.execute_script(
-                "return arguments[0].nextElementSibling;", article)
-
-        # clean up
-        # to give window time to close
-        driver.get("https://www.instagram.com/")
-    except:
-        traceback.print_exc()
-        print("Exception while handling instagram feed; closing driver...")
-    finally:
-        driver.close()
+        # sleep for period if set! otherwise exit
+        if period is None:
+            break
+        else:
+            print("Waiting", period, "seconds...")
+            try:
+                time.sleep(period)
+            except:
+                break
 
 
 if __name__ == "__main__":
-    run()
+    main(sys.argv[1:])
--- readme.md
+++ readme.md
@@ -1,3 +1,39 @@
 # ig-liker
 
-Likes your Instagram feed for you!
+Automatically likes your Instagram feed!
+
+## Setup
+
+Setup the Python environment with:
+
+```bash
+pip install requirements.txt
+```
+
+or, if using Conda, with:
+
+```bash
+conda env create --file=environment.yaml
+conda activate ig-liker
+```
+
+Run the script with:
+
+```bash
+python main.py
+```
+
+## Command-line options
+
+Option|Usage
+-|-
+`--username`|Instagram login. If not set, will be prompted.
+`--password`|Instagram password. If not set, will be prompted.
+`--stop-condition`|When the script should quit. Defaults to `5`. When more than `--stop-condition` posts have been encountered where each of them have already been liked, the script will quit.
+`--period`|If set, the script will wait for `--period` seconds before running again. This will happen indefinitely.
+
+The following flags should not be given a value
+Flag|Usage
+-|-
+`--headless`|If set, the chromedriver will be run in headless mode. This may cause additional errors, but will not launch a GUI.
+
